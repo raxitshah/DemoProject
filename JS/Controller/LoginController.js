d6c1539915d946d966b1fd3118049f21f5c6903a
raxitshah <raxit.gtl@gmail.com>
2015-09-23 20:26:03 +0530

    Committed initial code changes

---
+++ JS/Controller/LoginController.js
@@ -0,0 +1,13 @@
+﻿myapp.controller('LoginController', ['$scope',
+    function ($scope) {
+
+        $scope.username = "";
+        $scope.password = "";
+        $scope.Login = function () {
+            if ($scope.username == 'nosql' && $scope.password == 'html5')
+                alert('Welcome to the hell');
+            else {
+                alert('Invalid user');
+            }
+        };
+    }]);
